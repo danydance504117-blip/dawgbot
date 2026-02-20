7fc3ac494f0902ba2ccc57b6fd8c3d70c9879311
danydance504117-blip <danydance504117@gmail.com>
2026-02-20 20:50:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -112,8 +112,10 @@ async def btn_brezglivost(message: Message):
     await message.answer_photo(image, caption=text_song_BREZLIVOST, reply_markup=types.ReplyKeyboardRemove())
 
 async def main():
+    await start_server()
     await dp.start_polling(bot)
 
+if __name__ == "__main__":
+    asyncio.run(main())
 
-asyncio.run(main())
 
